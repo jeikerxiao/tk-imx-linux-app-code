29f51d2eb0867443cc057d0e87eb1e0317cbd20e
jeikerxiao <jeiker@126.com>
2026-07-16 20:19:02 +0800

    init linux app code
    init linux app code

---
+++ 33_mqtt/mqtt_prj/build/CMakeFiles/mqttClient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mqttClient.dir/mqttClient.c.o"
+  "bin/mqttClient"
+  "bin/mqttClient.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mqttClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
